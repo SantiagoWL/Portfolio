42092a54cdafe75a7c51213f276c1f831cf15c09
SantiagoWL <santuagowl@gmail.com>
2023-03-14 00:31:54 -0600

    First test - Portfolio
    This is the first part of my portfolio, just checking how it is looking.

---
+++ data.js
@@ -0,0 +1,39 @@
+export const tecData = [
+    {
+        name: `HTML`,
+        experience: `Around 1 year experience`,
+    },
+    {
+        name: `CSS`,
+        experience: `Around 1 year experience`,
+    },
+    {
+        name: `Javascript`,
+        experience: `Around 1 year experience`,
+    }
+
+]
+
+export const projectsData = [
+    {
+        image: "/images/Scrimbafyme.png",
+        nameProject: "SCIMBAFY.ME",
+        tecUsed: ["HTML", "CSS"],
+        url: "https://app.netlify.com/sites/scrimbafyme/overview",
+        github: "https://github.com/SantiagoWL/Scrimbafy-Me"
+    },
+    {
+        image: "/images/catfeelings.png",
+        nameProject: "CAT MOOD",
+        tecUsed: ["HTML", "CSS", "JAVASCRIPT"],
+        url: "https://app.netlify.com/sites/cat-feelings/overview",
+        github: "https://github.com/SantiagoWL/Cat-Mood"
+    },
+    {
+        image: "/images/Twimba.png",
+        nameProject: "TWIMBA",
+        tecUsed: ["HTML", "CSS", "JAVASCRIPT"],
+        url: "https://twimba-scrimba.netlify.app/",
+        github: "https://github.com/SantiagoWL/Twimba"
+    }
+]
---
+++ index.js
@@ -0,0 +1,5 @@
+import { tecData } from './data.js'
+import { projectsData } from './data.js'
+
+
+console.log(projectsData[0].tecUsed[0])
